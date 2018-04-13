92377a337c95987b9850ea6b9b01eacb9811adf6
Chris Brent <chris.brent@gmail.com>
2018-04-13 16:37:46 -0700

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,10 @@ var cssnano = require('gulp-cssnano');
 var useref = require('gulp-useref');
 var imagemin = require('gulp-imagemin');
 var rsync = require('gulp-rsync')
-var settings = require("./settings.js")
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+
+var settings = require('./config.json');
 
 gulp.task('browserSync', function() {
   console.log('Starting browserSync');
@@ -20,14 +23,69 @@ gulp.task('browserSync', function() {
   gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
-
 gulp.task('nunjucksTask', nunjucksTask);
 gulp.task('default', defaultTask);
 gulp.task('userefTask', userefTask);
 gulp.task('imageTask', imageTask);
 gulp.task('watch', gulp.series('browserSync'));
-gulp.task('build', gulp.series('nunjucksTask', 'userefTask', 'imageTask'));
-gulp.task('deploy', function() {
+gulp.task('build', buildTask);
+gulp.task('rsync', rsyncTask);
+gulp.task('deploy', gulp.series('build', 'rsync'));
+
+gulp.task('bump-version', function () {
+  return gulp.src(['./package.json'])
+    .pipe(bump({type: "patch"}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+
+  function getPackageJsonVersion () {
+    // We parse the json file instead of using require because require caches
+    // multiple calls so the version number won't be updated
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('release', function (callback) {
+  gulp.series(
+    'bump-version',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    })(callback);
+});
+
+function buildTask(done){
+  return gulp.series('nunjucksTask', 'userefTask', 'imageTask')(done);
+}
+
+function rsyncTask(done) {
+  console.log('Settings: ' + settings);
   return gulp.src('dist/**')
     .pipe(rsync({
       root:'dist',
@@ -35,7 +93,7 @@ gulp.task('deploy', function() {
       destination: settings.rsync.destination,
       username: settings.rsync.username
     }));
-});
+}
 
 function watchTask(done){
   gulp.watch(['app/css/*.css', 'app/**/*.nunjucks'], nunjucksTask);
@@ -71,5 +129,5 @@ function imageTask(done){
 
 function defaultTask(done){
   console.log('Starting defaultTask');
-  return gulp.series('watch');
+  return gulp.series('watch')(done);
 }
